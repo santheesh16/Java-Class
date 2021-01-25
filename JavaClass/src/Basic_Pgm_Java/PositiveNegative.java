06d4b79849140120d70eda35a72f67a4424bb708
Santheesh <55356327+santheesh16@users.noreply.github.com>
2021-01-25 21:39:54 +0530

    3 question completed conditional statement

--- JavaClass/src/Basic_Pgm_Java/PositiveNegative.java
+++ JavaClass/src/Basic_Pgm_Java/PositiveNegative.java
@@ -1,24 +1,42 @@
 package Basic_Pgm_Java;
 
+
+//1.Write the program to get a number from the user print the number is positive or negative
 import java.util.Scanner;
 
+@SuppressWarnings("resource")
 public class PositiveNegative {
+	
 	public static void main(String[] args) {
 		int count = 0;
+		System.out.println("Enter the number: ");
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
-		if(num > 0) {
+		System.out.println("Using if..else");
+		if (num > 0) {
 			System.out.println("The Given is Positive");
-			
-		}else {
+			count = 1;
+		} else {
 			System.out.println("The Given is Negative");
+			count = 2;
 		}
+		System.out.println("Using else..if");
+		if (num > 0) {
+			System.out.println("The Given is Positive");
+		} else if (num < 0) {
+			System.out.println("The Given is Negative");
+		} else {
+			System.out.println("The Given is Zero");
+		}
+		System.out.println("Using Switch Case");
 		switch (count) {
 		case 1: {
 			System.out.println("The Given is Positive");
+			break;
 		}
 		case 2:
 			System.out.println("The Given is Negative");
+			break;
 		}
 	}
 }
